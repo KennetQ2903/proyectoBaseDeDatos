3250f1afc6553cac5ba03076b49e6ace418741a0
Francisco-Back <fbacks@miumg.edu.gt>
2023-10-17 20:17:37 -0600

    VistaTabla

--- src/main/java/com/proyecto/kennet/eduardo/raul/francisco/proyectobasededatos1/Classes/Cliente.java
+++ src/main/java/com/proyecto/kennet/eduardo/raul/francisco/proyectobasededatos1/Classes/Cliente.java
@@ -28,4 +28,29 @@ public class Cliente {
     private Date fecha_creacion;
     private Date fecha_mod;
     private int estado;
+
+    public Cliente(int id_cliente, String primer_nombre, String segundo_nombre, String primer_apellido, String segundo_apellido, String otros_apellidos, String calle, String colonia, String zona, String ciudad, int municipio, int departamento, String codigo_postal, String telefono, String email, String nit, String dpi, int usuario_creacion, int usuario_mod, Date fecha_creacion, Date fecha_mod, int estado) {
+        this.id_cliente = id_cliente;
+        this.primer_nombre = primer_nombre;
+        this.segundo_nombre = segundo_nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+        this.otros_apellidos = otros_apellidos;
+        this.calle = calle;
+        this.colonia = colonia;
+        this.zona = zona;
+        this.ciudad = ciudad;
+        this.municipio = municipio;
+        this.departamento = departamento;
+        this.codigo_postal = codigo_postal;
+        this.telefono = telefono;
+        this.email = email;
+        this.nit = nit;
+        this.dpi = dpi;
+        this.usuario_creacion = usuario_creacion;
+        this.usuario_mod = usuario_mod;
+        this.fecha_creacion = fecha_creacion;
+        this.fecha_mod = fecha_mod;
+        this.estado = estado;
+    }
 }
--- src/main/java/com/proyecto/kennet/eduardo/raul/francisco/proyectobasededatos1/Controllers/ClientesController.java
+++ src/main/java/com/proyecto/kennet/eduardo/raul/francisco/proyectobasededatos1/Controllers/ClientesController.java
@@ -1,22 +1,25 @@
 package com.proyecto.kennet.eduardo.raul.francisco.proyectobasededatos1.Controllers;
 
 import com.proyecto.kennet.eduardo.raul.francisco.proyectobasededatos1.Classes.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 import java.net.URL;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
 
 
 public class ClientesController implements Initializable {
-
+Cliente selectCliente=null;
+    Cliente cliente=null;
     @FXML
     private ComboBox<Departamento> BoxDepartamento;
     @FXML
@@ -35,23 +38,88 @@ public class ClientesController implements Initializable {
     private TextField TextTelefono;
     private TextField TextZona;
     private TextField TextoApellido;
+    @FXML
+    private Button ButtonEditar;
+    @FXML
+    private Button ButtonEliminar;
+    @FXML
+    private TableView<Cliente> TablaClientes;
+
+
+    @FXML
+    private TableColumn<Cliente, Integer> TableID;
+    @FXML
+    private TableColumn<Cliente, String> TableNombre;
+    @FXML
+    private TableColumn<Cliente, String> TableDPI;
+    @FXML
+    private TableColumn<Cliente, String> TableEmail;
+    @FXML
+    private TableColumn<Cliente, String > TableNit;
+    @FXML
+    private TableColumn<Cliente, String> TableTelefono;
+
     ObservableList<Departamento> departamentosList = FXCollections.observableArrayList();
     ObservableList<Municipio> municipiosList = FXCollections.observableArrayList();
-   public String SQL;
-   DBConnection conn=new DBConnection();
 
-    Cliente cliente=new Cliente();
+    /**por problemas con la base de datos Dejo aqui la Consultas SQL para que puedan ser modificadas con mayor
+     * facilidad ya que pueden no necesitar el SYS.
+    **/
+   public String SQLDepartamentos="SELECT * FROM SYS.DEPARTAMENTO";;
+    public String SQLMunicipios= "SELECT * FROM SYS.MUNICIPIO WHERE DEPARTAMENTO = ?";;
+    public String SQLInsert;
+    public String SQLDelete;
+    public String SQLUpdate;
+    public String SQLSetClientes="SELECT * FROM SYS.CLIENTE";
+    // Variable de conexión a bd
+   DBConnection conn=new DBConnection();
+   //Crear el Objeto Alerta
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    //Creo los Botones para las Alertas
+    ButtonType buttonTypeYes = new ButtonType("Sí");
+    ButtonType buttonTypeNo = new ButtonType("No");
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-       setDepartamentos();
 
+        //Captura la seleccion de la filas de las tablas
+
+
+        try {
+            setDepartamentos();
+            setClientes();
+            ButtonEditar.setDisable(true);
+            ButtonEliminar.setDisable(true);
+            TablaClientes.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Cliente>() {
+                @Override
+                public void changed(ObservableValue<? extends Cliente> observableValue, Cliente oldcliente, Cliente newcliente) {
+
+                    if (newcliente != null) {
+                        selectCliente = newcliente;
+                        ButtonEditar.setDisable(false);
+                        ButtonEliminar.setDisable(false);
+
+                    } else {
+                        ButtonEditar.setDisable(true);
+                        ButtonEliminar.setDisable(true);
+                    }
+
+                }
+            });
+        } catch (Exception e) {
+            Mensaje("No se puedo Obtener Lista", String.valueOf(e));
+            System.out.println(e);
+
+
+        }
     }
     //Metodos setDepartamentos para llenar los Combox
     private void setDepartamentos(){
         try {
-            SQL="SELECT * FROM SYS.DEPARTAMENTO";
-            PreparedStatement statement = conn.getConnection().prepareStatement(SQL);
+
+            PreparedStatement statement = conn.getConnection().prepareStatement(SQLDepartamentos);
             ResultSet rs = statement.executeQuery();
             System.out.println(rs);
             if (rs != null) {
@@ -87,8 +155,8 @@ public class ClientesController implements Initializable {
         Departamento value = BoxDepartamento.getValue();
         if (value != null) {
             //el símbolo de interrogación indica donde reemplazaremos el valor de una variable para no concatenar
-            SQL= "SELECT * FROM SYS.MUNICIPIO WHERE DEPARTAMENTO = ?";
-            PreparedStatement statement = conn.getConnection().prepareStatement(SQL);
+
+            PreparedStatement statement = conn.getConnection().prepareStatement(SQLMunicipios);
             // Asignamos un valor al primer"?", que está en la query, en este caso él, id del departamento para obtener los municipios de ese departamento
             statement.setInt(1, value.getID_DEPARTAMENTO());
             System.out.println(value.getID_DEPARTAMENTO());
@@ -113,7 +181,61 @@ public class ClientesController implements Initializable {
             System.out.println(e);
         }
     }
+    /**
+     * Metodo utilizado para obtener el listado de Clientes y asignarlos
+     * a la TableView de usuarios
+     */
+private void setClientes(){
+        try {
+           TableID.setCellValueFactory(new PropertyValueFactory<>("id_cliente"));
+           TableNombre.setCellValueFactory(new PropertyValueFactory<>("primer_nombre"));
+            TableEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+           TableNit.setCellValueFactory(new PropertyValueFactory<>("nit"));
+           TableTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+           TableDPI.setCellValueFactory(new PropertyValueFactory<>("dpi"));
+           PreparedStatement statement =conn.getConnection().prepareStatement(SQLSetClientes);
+           ResultSet rs= statement.executeQuery();
+           if(rs != null){
+               ObservableList<Cliente> ListaClientes= FXCollections.observableArrayList();
+               while (rs.next()){
+                   Cliente client= new Cliente(
+                   rs.getInt("id_cliente"),
+                   rs.getString("primer_nombre"),
+                   rs.getString("segundo_nombre"),
+                   rs.getString("primer_apellido"),
+                   rs.getString("segundo_apellido"),
+                   rs.getString("otros_apellidos"),
+                   rs.getString("calle"),
+                   rs.getString("colonia"),
+                   rs.getString("zona"),
+                   rs.getString("ciudad"),
+                   rs.getInt("municipio"),
+                   rs.getInt("departamento"),
+                   rs.getString("codigo_postal"),
+                   rs.getString("telefono"),
+                   rs.getString("email"),
+                   rs.getString("nit"),
+                   rs.getString("dpi"),
+                   rs.getInt("usuario_creacion"),
+                   rs.getInt("usuario_mod"),
+                   rs.getDate("fecha_creacion"),
+                   rs.getDate("fecha_mod"),
+                   rs.getInt("estado")
+                   );
+                   ListaClientes.add(client);
+               }
+               TablaClientes.setItems(ListaClientes);
+           }
 
+
+
+        }catch (SQLException e){
+            Mensaje("Falla BD Lista Clientes Tabla", String.valueOf(e));
+
+        }
+
+
+}
 
 
     @FXML
@@ -131,6 +253,7 @@ public class ClientesController implements Initializable {
                 PreparedStatement insert= conn.getConnection().prepareStatement("insert into ");
 
             }else{
+
          Mensaje("Error Campos Vacios", "Campos Vacios al ingreso de datos");
             }
 
@@ -144,7 +267,35 @@ public class ClientesController implements Initializable {
     @FXML
     void LimpiarLabel(ActionEvent event) {
         System.out.println("Hola 2");
+        Alerta("Limpiar Campos", "Se limpirar todo los campos","¿Desea Continuar?");
+        alert.showAndWait().ifPresent(result ->{
+            if(result == buttonTypeYes ){
+              Te
+            }else if(result == buttonTypeNo){
+                System.out.println("Eligio no");
+
+            }
+
+        });
         
+    }
+
+    @FXML
+    void ButtonEditar(ActionEvent event) {
+
+    }
+    @FXML
+    void ButtonEliminar(ActionEvent event) {
+alert.showAndWait().ifPresent(result ->{
+    Alerta("Eliminar Usuario", "Usuario sera Eliminado de BD","¿Desea Continuar?");
+    if(result == buttonTypeYes ){
+        System.out.println("Eligio Si");
+    }else if(result == buttonTypeNo){
+        System.out.println("Eligio no");
+
+    }
+
+});
     }
 
 
@@ -154,13 +305,19 @@ public class ClientesController implements Initializable {
     **/
 public void Mensaje(String titulo, String mensaje){
     Notifications.create()
-
             .title(titulo)
             .text(mensaje)
             .hideAfter(Duration.seconds(3))
             .show();
-}
 
+}
+public void Alerta(String Titulo, String Header, String Contenido ){
+    alert.setTitle(Titulo);
+    alert.setHeaderText(Header);
+    alert.setContentText(Contenido);
+    // Configura los botones
+    alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+}
 
 
 }
